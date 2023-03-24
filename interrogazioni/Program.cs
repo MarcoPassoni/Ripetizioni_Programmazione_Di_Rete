08fbc5ca0fde918f0508b1802bd05b19fb8a3bab
MarcoPassoni <91603717+MarcoPassoni@users.noreply.github.com>
2023-03-24 17:44:37 +0100

    Add project files.

---
+++ interrogazioni/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace interrogazioni
+{
+    internal class Program
+    {
+        public static HttpClient client;
+
+        static async Task Main(string[] args)
+        {
+            client = new HttpClient();
+            string path = "https://jsonplaceholder.typicode.com/ps";
+            var response = await client.GetAsync(path);
+            if (!response.IsSuccessStatusCode)
+            {
+                return;
+            }
+
+            List<User>? userList = await response.Content.ReadFromJsonAsync<List<User>>();
+            foreach (var item in userList)
+            {
+                Console.WriteLine(item.UserId);
+            }
+        }
+    }
+}
