95381dab29492224f2d7713611d0c55b2ea7094a
MarcoPassoni <91603717+MarcoPassoni@users.noreply.github.com>
2023-03-30 22:00:31 +0200

    update

---
+++ 12/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _12
+{
+    internal class Program
+    {
+        static HttpClient client= new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            string path = "https://www.ilgiorno.it/";
+            var response = await client.GetAsync(path);
+            if (!response.IsSuccessStatusCode)
+            {
+                return;
+            }
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+            File.WriteAllText("./file.html", result);
+        }
+    }
+}
